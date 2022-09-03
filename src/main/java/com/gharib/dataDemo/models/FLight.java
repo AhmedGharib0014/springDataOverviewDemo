680aaa5e1699941e097a42000b7edcb3fae9a65b
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-09-03 15:44:39 +0200

    create entity , saving it with entity manager , query all entities ans assert every thing is ok

---
+++ src/main/java/com/gharib/dataDemo/models/FLight.java
@@ -0,0 +1,61 @@
+package com.gharib.dataDemo.models;
+
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class FLight {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private  String origin;
+    private  String destination;
+    private LocalDateTime scheduleAt;
+
+
+    @Override
+    public String toString() {
+        return "FLight{" +
+            "id=" + id +
+            ", origin='" + origin + '\'' +
+            ", destination='" + destination + '\'' +
+            ", scheduleAt=" + scheduleAt +
+            '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getScheduleAt() {
+        return scheduleAt;
+    }
+
+    public void setScheduleAt(LocalDateTime scheduleAt) {
+        this.scheduleAt = scheduleAt;
+    }
+}
--- src/test/java/com/gharib/dataDemo/DataDemoApplicationTests.java
+++ src/test/java/com/gharib/dataDemo/DataDemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.gharib.dataDemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.gharib.dataDemo.models.FLight;
+import java.util.List;
 
-@SpringBootTest
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 class DataDemoApplicationTests {
 
+	@Autowired
+	private EntityManager entityManager;
+
 	@Test
-	void contextLoads() {
+	void testSavingData() {
+
+		//save data
+		FLight fLight = new FLight();
+		fLight.setOrigin("test");
+		fLight.setDestination("test2");
+		entityManager.persist(fLight);
+
+
+		// query data
+		final TypedQuery<FLight> result = entityManager.createQuery("SELECT f FROM FLight f",FLight.class);
+		final List<FLight> flights = result.getResultList();
+
+		Assertions.assertThat(flights).hasSize(1).first().isEqualTo(fLight);
+
+
 	}
 
 }
