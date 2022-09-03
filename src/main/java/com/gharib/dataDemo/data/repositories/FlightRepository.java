a897022964d9d2d5a653fa84aaf94ebf5fc71557
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-09-03 17:09:54 +0200

    derived quesries creating and test

--- src/main/java/com/gharib/dataDemo/data/repositories/FlightRepository.java
+++ src/main/java/com/gharib/dataDemo/data/repositories/FlightRepository.java
@@ -1,8 +1,17 @@
 package com.gharib.dataDemo.data.repositories;
 
 import com.gharib.dataDemo.data.models.FLight;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface FlightRepository extends CrudRepository<FLight,Long> {
+    List<FLight> findByOrigin(String origin);
+
+    List<FLight> findByOriginAndDestination(String origin,String destination);
+
+
+    List<FLight> findByDestinationIn(String...des);
+
+    List<FLight> findByOriginIgnoreCase(String origin);
 }
--- src/test/java/com/gharib/dataDemo/DataDemoApplicationTests.java
+++ src/test/java/com/gharib/dataDemo/DataDemoApplicationTests.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,6 +21,12 @@ class DataDemoApplicationTests {
 
 	@Autowired
 	private FlightRepository flightRepository;
+
+
+	@BeforeEach
+	public void setUp(){
+		flightRepository.deleteAll();
+	}
 
 	@Test
 	void testSavingData() {
@@ -63,4 +70,84 @@ class DataDemoApplicationTests {
 
 	}
 
+	@Test
+	public  void testDerivedByField(){
+		FLight fLight1 = createFlightByOrigin("origin");
+		FLight fLight2 = createFlightByOrigin("origin");
+		FLight fLight3 = createFlightByOrigin("origin2");
+
+
+		flightRepository.save(fLight1);
+		flightRepository.save(fLight2);
+		flightRepository.save(fLight3);
+
+		List<FLight> flights = flightRepository.findByOrigin("origin");
+
+		Assertions.assertThat(flights).hasSize(2);
+
+	}
+
+	@Test
+	public  void testDerivedLogicAnd(){
+		FLight fLight1 = createFlightByOriginAndDestination("origin","des");
+		FLight fLight2 = createFlightByOriginAndDestination("origin","de2");
+		FLight fLight3 = createFlightByOriginAndDestination("origin","de2");
+
+		flightRepository.save(fLight1);
+		flightRepository.save(fLight2);
+		flightRepository.save(fLight3);
+
+		List<FLight> flights = flightRepository.findByOriginAndDestination("origin","des");
+
+		Assertions.assertThat(flights).hasSize(1);
+
+	}
+
+	@Test
+	public  void testDerivedLogicOr(){
+		FLight fLight1 = createFlightByOriginAndDestination("origin","des");
+		FLight fLight2 = createFlightByOriginAndDestination("origin","de2");
+		FLight fLight3 = createFlightByOriginAndDestination("origin","de2");
+
+		flightRepository.save(fLight1);
+		flightRepository.save(fLight2);
+		flightRepository.save(fLight3);
+
+		List<FLight> flights = flightRepository.findByDestinationIn("de2","des");
+
+		Assertions.assertThat(flights).hasSize(3);
+
+	}
+
+
+	@Test
+	public  void testDerivedLogicIgnoreCase(){
+		FLight fLight1 = createFlightByOriginAndDestination("origin","des");
+		FLight fLight2 = createFlightByOriginAndDestination("origin","de2");
+		FLight fLight3 = createFlightByOriginAndDestination("origin","de2");
+
+		flightRepository.save(fLight1);
+		flightRepository.save(fLight2);
+		flightRepository.save(fLight3);
+
+		List<FLight> flights = flightRepository.findByOriginIgnoreCase("origin");
+
+		Assertions.assertThat(flights).hasSize(3);
+
+	}
+
+	public  FLight createFlightByOrigin(String origin){
+		FLight fLight = new FLight();
+		fLight.setOrigin(origin);
+		fLight.setDestination("any thing");
+		return  fLight;
+	}
+
+
+	public  FLight createFlightByOriginAndDestination(String origin,String des){
+		FLight fLight = new FLight();
+		fLight.setOrigin(origin);
+		fLight.setDestination(des);
+		return  fLight;
+	}
 }
